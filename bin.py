defbc5920e2ba9e3ebb95dbf4ee065630965442a
Abhishek Anand <anandabhishek01555@gmail.com>
2020-10-01 11:43:09 +0530

    Update bin.py

--- bin.py
+++ bin.py
@@ -1,14 +1,8 @@
-# A simple way to convert a number from decimal to binary.
-
-x=int(input())
-x=bin(x)
-print(x[2:])
-
-# n= int(input())
-# li=list()
-# while n>0:
-#     r = n%2
-#     li.append(r)
-#     n=n//2 
-# li.reverse()
-# print("".join(list(map(str,li))))
+n= int(input())
+li=list()
+while n>0:
+    r = n%2
+    li.append(r)
+    n=n//2 
+li.reverse()
+print("".join(list(map(str,li))))
