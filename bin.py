1171ade7679dcff44a488b8d4ef70f269d3ea4d2
Abhishek Anand <anandabhishek01555@gmail.com>
2020-10-01 10:50:10 +0530

    Update bin.py

--- bin.py
+++ bin.py
@@ -1,8 +1,14 @@
-n= int(input())
-li=list()
-while n>0:
-    r = n%2
-    li.append(r)
-    n=n//2 
-li.reverse()
-print("".join(list(map(str,li))))
+
+x=int(input())
+x=bin(x)
+print(bin(x[2:]))
+
+
+# n= int(input())
+# li=list()
+# while n>0:
+#     r = n%2
+#     li.append(r)
+#     n=n//2 
+# li.reverse()
+# print("".join(list(map(str,li))))
